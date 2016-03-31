d7bee4000ff6cae0438170f7b4a6f7ba0b4edf0a
Solomon Huang <kaichanh@gmail.com>
2016-03-31 13:15:15 +0800

    make clean for batch

--- make.bat
+++ make.bat
@@ -1,1 +1,12 @@
+@echo off
+
+if "%1" == "clean" (
+  echo Clean watermark.exe and watermark.spec
+  del watermark.exe watermark.spec
+  echo Done
+  goto end
+)
+
 pyinstaller --onefile --distpath . src\watermark.py
+
+:end
